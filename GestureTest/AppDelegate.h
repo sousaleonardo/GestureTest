b0f596c5e411c2a2092771fe8c52f0bb80c39a92
LEONARDO DE SOUSA MENDES <leonardo.smendes1@CAS390285M441.local>
2014-03-10 15:36:05 -0300

    Initial Commit

---
+++ GestureTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GestureTest
+//
+//  Created by LEONARDO DE SOUSA MENDES on 10/03/14.
+//  Copyright (c) 2014 LEONARDO DE SOUSA MENDES. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
